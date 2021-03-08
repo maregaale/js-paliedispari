f677bbeff4cf56162af1ef238c40d41d8016dcc6
Alessandro Marega <ale_marega@hotmail.it>
2021-03-08 15:36:33 +0100

    aggiunto prompt e la funzione generica che torna parola al contrario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+// esercizio numero 1: Parola Palindroma
 
+// 1. chiedo all'utente una parola
+var parolaUtente = prompt("inserisci una parola");
+
+// 2. Creare una funzione per capire se la parola inserita è palindroma
+
+// creo funzione che torni una parola al contrario
+function wordReverse(word) {
+  // variabili
+  var reverseWord = "";
+  // attraverso la stringa e ritorno l'inverso
+  for (var i = (word.length - 1); i >= 0 ; i--) {
+    // wordReverse += word[i];
+    reverseWord += word[i];
+  }
+  return reverseWord;
+}
+
+console.log(wordReverse(parolaUtente));
