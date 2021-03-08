4acd696be795c65a86e97f4b08045c0dfda95523
Alessandro Marega <ale_marega@hotmail.it>
2021-03-08 17:37:24 +0100

    aggiunta stampa a schermo del risultato di entrambi gli esercizi

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
-// // esercizio numero 1: Parola Palindroma
-//
-// // 1. chiedo all'utente una parola
-// var parolaUtente = prompt("inserisci una parola");
-//
-// // 2. Creare una funzione per capire se la parola inserita è palindroma
-//
-// // creo funzione che torni una parola al contrario
-// function wordReverse(word) {
-//   // variabili
-//   var reverseWord = "";
-//   // attraverso la stringa e ritorno l'inverso
-//   for (var i = (word.length - 1); i >= 0 ; i--) {
-//     // wordReverse += word[i];
-//     reverseWord += word[i];
-//   }
-//   return reverseWord;
-// }
-//
-// // uso la funzione e controllo se parola inserita è Palindroma
-// if (parolaUtente == wordReverse(parolaUtente)) {
-//   console.log("la parola è palindroma");
-// } else {
-//   console.log("la parola non è palindroma");
-// }
+// esercizio numero 1: Parola Palindroma
+
+// 1. chiedo all'utente di inserire una parola
+var parolaUtente = document.getElementById('word-input');
+
+// 2. Creare una funzione per capire se la parola inserita è palindroma
+// creo funzione che torni una parola al contrario
+function wordReverse(word) {
+  // variabili
+  var reverseWord = "";
+  // attraverso la stringa e ritorno l'inverso
+  for (var i = (word.length - 1); i >= 0 ; i--) {
+    // wordReverse += word[i];
+    reverseWord += word[i];
+  }
+  return reverseWord;
+}
+
+button.addEventListener("click",
+function () {
+  // uso la funzione e controllo se parola inserita è Palindroma
+  if (parolaUtente.value != "") {
+    if (parolaUtente.value == wordReverse(parolaUtente.value)) {
+      document.getElementById('first-result').innerHTML = "la parola è palindroma";
+    } else {
+      document.getElementById('first-result').innerHTML = "la parola non è palindroma";
+    }
+  } else {
+    alert("inserisci una parola nel gioco 1");
+  }
+}
+);
+
 
 
 // esercizio numero 2: Pari e dispari
@@ -31,8 +39,6 @@ var evenOddValues = document.getElementsByClassName('even-odd-values');
 var numberUser = document.getElementById('number');
 var btn = document.getElementById('btn');
 var numberComputer = randomGenerator(1, 5);
-// var result = document.getElementsById('result');
-
 
 // funzione per generare numero random da x a y
 function randomGenerator(x, y) {
@@ -65,11 +71,10 @@ btn.addEventListener("click",
 
       // stampo risultato se la somma e il numero random sono entrambi pari o dispari
       if (sumIsEvenOrOdd(numberComputer, parseInt(numberUser.value)) == val) {
-        document.getElementById('result').innerHTML = "il numero del computer è " + numberComputer + ", hai vinto!!";
+        document.getElementById('second-result').innerHTML = "il numero del computer è " + numberComputer + ", hai vinto!!";
       } else {
-        document.getElementById('result').innerHTML = "il numero del computer è " + numberComputer + ", hai perso";
+        document.getElementById('second-result').innerHTML = "il numero del computer è " + numberComputer + ", hai perso";
       }
-
     } else {
       alert("inserisci un numero da 1 a 5");
     }
